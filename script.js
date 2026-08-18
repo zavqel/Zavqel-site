3847391f62c66ade97ff737d878108fc6ae121fb
zavqel <zavqelparfums@gmail.com>
2026-08-18 20:34:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-const t=document.querySelector('.menu-toggle'),m=document.querySelector('.menu');if(t&&m)t.onclick=()=>{let o=m.classList.toggle('open');t.setAttribute('aria-expanded',o)};
+const t=document.querySelector('.menu-toggle'),m=document.querySelector('.menu');
+if(t&&m)t.onclick=()=>{const o=m.classList.toggle('open');t.setAttribute('aria-expanded',o)};
